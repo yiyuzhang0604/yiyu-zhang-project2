ede0a8b2382373bc5f60de93e1a98cdcf586647a
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-03-28 17:02:50 -0700

    add src

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { ReactDOM } from 'react-DOM';
+import Home from '/Home.jsx'; 
+import '/index.css'; 
+
+
+
+ReactDOM.render(
+    <Home />, 
+    document.getElementById('root')
+); 
+
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'; 
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <div class="title">
+                Welcome to Wordle! 
+            </div>
+        </div>
+    ); 
+}
+
+export default Home; 
