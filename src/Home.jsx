cbc2620ed9803d3e180e495942f51ec15fb139b8
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-03-28 18:04:32 -0700

    add instruction

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,36 @@
 import './Home.css'; 
 import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import './instruction'; 
+
+const {Link, BrowserRouter} = window.ReactRouterDOM
 
 function Home() {
     return (
         <div>
+            <BrowserRouter>
+
+            
             <div class="title">
                 Welcome to Wordle! 
             </div>
+
+            
+            <button>
+                <Link to='/instruction'>Insructions on how to play: </Link>
+            </button>
+            
+
+            <div class="title">
+                Choose your difficulty
+            </div>
+
+            <button>Easy</button>
+            <button>Medium</button>
+            <button>Hard</button>
+            </BrowserRouter>
+
+
         </div>
     ); 
 }
---
+++ src/instruction.jsx
@@ -0,0 +1,22 @@
+
+import { useState } from 'react';
+
+
+
+
+function Instruction() {
+    return (
+        <div>
+        <h1>
+            Instruction on how to play
+        </h1>
+        <h2>Easy game</h2>
+            <p>Try to guess the correct 5 letter words in 7 prompts!</p>
+
+        
+
+        </div>
+    ); 
+}
+
+export default Home; 
