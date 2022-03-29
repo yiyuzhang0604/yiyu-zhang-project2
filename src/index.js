9b315c95c24535abbf78eb1b57328972c6d1c4d5
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-03-28 17:46:44 -0700

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { ReactDOM } from 'react-DOM';
-import Home from '/Home.jsx'; 
-import '/index.css'; 
+import Home from './Home.jsx'; 
+import './index.css'; 
 
 
 
