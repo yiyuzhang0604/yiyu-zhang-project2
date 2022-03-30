c6b93dd3fa9361b910800674db54f85dc9186c37
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-03-29 20:13:53 -0700

    add home page and instruction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './Home'; 
 import Instruction from './Instruction';
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styles from './Home.css'; 
